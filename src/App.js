e83d45386cd5371ea832ef1dbefddb0ab00003fd
ROHAN VISHWAKARMA <90124142+vishwarohan@users.noreply.github.com>
2025-01-05 09:56:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,16 +6,23 @@ import EditTaskPage from "./pages/EditTaskPage";
 import { Provider } from "react-redux";
 import store from "./store";
 
+// The `App` component serves as the root component of the application.
 const App = () => (
+  // Wrapping the entire application with the Redux `Provider` to make the store accessible to all components
   <Provider store={store}>
+    {/* Setting up the React Router for client-side routing */}
     <Router>
+      {/* Define the routes for the application */}
       <Routes>
+        {/* Home Page route */}
         <Route path="/" element={<HomePage />} />
+        {/* Add Task Page route */}
         <Route path="/add" element={<AddTaskPage />} />
+        {/* Edit Task Page route with dynamic `id` parameter */}
         <Route path="/edit/:id" element={<EditTaskPage />} />
       </Routes>
     </Router>
   </Provider>
 );
 
-export default App;
+export default App; 
